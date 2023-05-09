03c82c3d147ce6d5f82710176addc196a3d1ebf6
Todi Olubode <todimu18@gmail.com>
2023-05-09 15:26:45 +0100

    hi

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
-
+app.get('/restaurants', async (req, res) => {
+    try {
+        const restaurants = await Restaurant.findAll();
+        res.json(restaurants)
+    }
+    catch(error){
+        next(error)
+    }
+})
 
 
 module.exports = app;
